f14055e2fd06f6eac2d51afaa134a92ecb91e823
andrij-K <andrijkurbatov@gmail.com>
2018-12-12 11:17:44 +0200

    Code

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,9 @@
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+ 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'docs'),
-		filename: 'js/main.js',
-	},
-	devServer: {
-		contentBase: path.join(__dirname, 'docs'),
-		compress: true,
-		port: 9000,
-		watchContentBase: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(sa|sc|c)ss$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader',
-					'sass-loader',
-				],
-			},
-			{
-				test: /\.m?js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-					},
-				},
-			},
-		],
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: "css/[name].css",
-		}),
-	],
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        filename: 'js/main.js',
+    },
 };
